a5206e590b24217fb99a5d773da7d1636d06df6b
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-25 17:54:46 -0300

    :sparkles: feat: create footer
    To display if the app is in production or development mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Footer from './components/Footer';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const enviroment = process.env.MODE === 'development';
+
   return (
     <div className="App">
       <StrangerThings />
+      {enviroment && <Footer />}
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      Em desenvolvimento
+    </footer>
+  );
+}
+
+export default Footer;
