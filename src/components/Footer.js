85bf3afe8c7730ad4f3833f29f78748e56126e41
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-25 18:29:32 -0300

    :lipstick: style(footer): make the footer visible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer
+      style={ {
+        position: 'fixed',
+        bottom: '30px',
+        color: 'white',
+        left: '50%',
+        fontSize: '2rem',
+        fontWeight: 700,
+        transform: 'translateX(-50%)',
+      } }
+    >
       Em desenvolvimento
     </footer>
   );
