d62e262b2953729d5f05049f586b492dee6a0ce8
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-25 18:49:51 -0300

    :bug: fix: add REACT_APP sufix to env variable
    I had to add REACT_APP sufix to the environment variable because React was not able to see it

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import Footer from './components/Footer';
 import StrangerThings from './components/StrangerThings';
 
-const enviroment = process.env.NODE_ENV === 'development';
+const enviroment = process.env.REACT_APP_ENVIRONMENT === 'development';
 
 function App() {
+  console.log(process.env);
+
   return (
     <div className="App">
       <StrangerThings />
